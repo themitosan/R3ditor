0cefba5de59204e5b80f5fc1b4cfb5dc6ca4a841
themitosan <juliodogers@gmail.com>
2019-10-29 18:57:05 -0300

    hunting...

--- App/js/update.js
+++ App/js/update.js
@@ -25,10 +25,10 @@ function forceUpdate(){
 }
 function checkForUpdates(){
 	if (navigator.onLine === true){
-		R3DITOR_downloadFile("https://raw.githubusercontent.com/themitosan/R3ditor/master/version.r3ditor", APP_PATH + "\\Update\\check.r3ditor");
+		R3DITOR_downloadFile('https://raw.githubusercontent.com/themitosan/R3ditor/master/version.r3ditor', APP_PATH + '\\Update\\check.r3ditor');
 		var wait = setInterval(function(){
 			if (DOWNLOAD_COMPLETE === true){
-				R3DITOR_readUpdate(APP_PATH + "\\Update\\check.r3ditor");
+				R3DITOR_readUpdate(APP_PATH + '\\Update\\check.r3ditor');
 				clearInterval(wait);
 			}
 		}, 50);
@@ -41,7 +41,7 @@ function R3DITOR_readUpdate(file){
 	var c = 3;
 	var update_info = [];
 	if (file === undefined || file === null){
-		addLog('warn', "Unable to read update info!");
+		addLog('warn', 'Unable to read update info!');
 	} else {
 		fs.readFileSync(file).toString().split('\n').forEach(function(line){ 
 			update_info.push(line); 
@@ -49,9 +49,9 @@ function R3DITOR_readUpdate(file){
 		if (parseInt(update_info[0]) > internal_version || TEST_RELEASE == true){
 			if (TEST_RELEASE === true){
 			RE3_LIVE_closeForm();
-				$("#aplicarUpdate").css({"display": "none"});
+				$("#aplicarUpdate").css({'display': 'none'});
 			} else {
-				$("#aplicarUpdate").css({"display": "inline"});
+				$("#aplicarUpdate").css({'display': 'inline'});
 			}
 			document.getElementById('new_version').innerHTML = update_info[1];
 			document.getElementById('new_version_title').innerHTML = update_info[2];
@@ -64,14 +64,14 @@ function R3DITOR_readUpdate(file){
 					newHTML = newHTML + update_info[c];
 					c++;
 				}
-				document.getElementById('updates_info').innerHTML = "<ul>" + newHTML + "</ul>";
+				document.getElementById('updates_info').innerHTML = '<ul>' + newHTML + '</ul>';
 			} else {
-				document.getElementById('updates_info').innerHTML = "<ul>" + update_info[3] + "</ul>";
+				document.getElementById('updates_info').innerHTML = '<ul>' + update_info[3] + '</ul>';
 			}
 			addLog('log', 'INFO - There is a new version of R3ditor avaliable! - Version: ' + update_info[1]);
 			R3DITORshowUpdate();
 		} else {
-			addLog('log', 'INFO - You are using the latest version of R3ditor. (Version: ' + APP_VERSION + ")");
+			addLog('log', 'INFO - You are using the latest version of R3ditor. (Version: ' + APP_VERSION + ')');
 			R3DITORcloseUpdate();
 		}
 	}
@@ -81,41 +81,41 @@ function R3DITOR_readUpdate(file){
 function R3DITOR_applyUpdate(){
 	if (navigator.onLine === true){
 		R3DITORshowUpdateProgress();
-		if (fs.existsSync(APP_PATH + "\\App\\check.r3ditor") === true){
-			fs.unlinkSync(APP_PATH + "\\App\\check.r3ditor");
+		if (fs.existsSync(APP_PATH + '\\App\\check.r3ditor') === true){
+			fs.unlinkSync(APP_PATH + '\\App\\check.r3ditor');
 		}
-		R3DITOR_movePercent(0, 1, "Downloading \"Master\" branch from GitHub...");
-		R3DITOR_downloadFile("https://codeload.github.com/themitosan/R3ditor/zip/master", APP_PATH + "\\Update\\master.zip");
+		R3DITOR_movePercent(0, 1, 'Downloading Master branch from GitHub...');
+		R3DITOR_downloadFile("https://codeload.github.com/themitosan/R3ditor/zip/master", APP_PATH + '\\Update\\master.zip');
 		var timer = setInterval(function(){
 			if (DOWNLOAD_COMPLETE === true){
 				clearInterval(timer);
-				R3DITOR_movePercent(0, 15, "Download Complete!");
+				R3DITOR_movePercent(0, 15, 'Download Complete!');
 				R3DITOR_update_0();
 			}
 		}, 50);
 	} else {
-		document.title = APP_NAME + " - ERROR!";
+		document.title = APP_NAME + ' - ERROR!';
 		addLog('error', 'ERROR - You are offline!');
-		R3DITOR_movePercent(0, 100, "ERROR - You are offline!");
+		R3DITOR_movePercent(0, 100, 'ERROR - You are offline!');
 		addLog('error', 'Check your internet status, Reload R3ditor and try again!');
 		if (DESIGN_ENABLE_ANIMS === true){
 			$("#btn_update_ok").fadeIn({duration: 200, queue: false});
 		} else {
 			$("#btn_update_ok").css({'display': 'inline'});
 		}
-		$("#progress_window").css({"top": "528px", "height": "74px"});
+		$("#progress_window").css({'top': '528px', 'height': '74px'});
 		scrollLog();
 	}
 }
 function R3DITOR_update_0(){
 	clearInterval(timer);
-	if (fs.existsSync(APP_PATH + "\\Update\\master.zip") === true){
-		R3DITOR_movePercent(0, 20, "Extracting Package...");
+	if (fs.existsSync(APP_PATH + '\\Update\\master.zip') === true){
+		R3DITOR_movePercent(0, 20, 'Extracting Package...');
 		runExternalSoftware(APP_PATH + "\\App\\tools\\7za.exe", ["x", APP_PATH + "\\Update\\master.zip", "-o" + APP_PATH + "\\Update\\Extract", "-aoa"]);
 		var timer = setInterval(function(){
 			if (EXTERNAL_APP_RUNNING === false){
 				clearInterval(timer);
-				R3DITOR_movePercent(0, 25, "Extract OK!");
+				R3DITOR_movePercent(0, 25, 'Extract OK!');
 				R3DITOR_update_1();
 			}
 		}, 50);
@@ -126,10 +126,10 @@ function R3DITOR_update_0(){
 	}
 }
 function R3DITOR_update_1(){
-	R3DITOR_movePercent(0, 26, "Removing zip file...");
-	fs.unlinkSync(APP_PATH + "\\Update\\master.zip");
-	R3DITOR_movePercent(0, 40, "Removing old files...");
-	deleteFolderRecursive(APP_PATH + "\\App");
+	R3DITOR_movePercent(0, 26, 'Removing zip file...');
+	fs.unlinkSync(APP_PATH + '\\Update\\master.zip');
+	R3DITOR_movePercent(0, 40, 'Removing old files...');
+	deleteFolderRecursive(APP_PATH + '\\App');
 	var timer = setInterval(function(){
 		if (EXTERNAL_APP_RUNNING === false){
 			clearInterval(timer);
@@ -138,9 +138,9 @@ function R3DITOR_update_1(){
 	}, 50);
 }
 function R3DITOR_update_2(){
-	if (fs.existsSync(APP_PATH + "\\App") == false){
-		fs.mkdirSync(APP_PATH + "\\App");
-		R3DITOR_movePercent(0, 50, "Moving the new files...");
+	if (fs.existsSync(APP_PATH + '\\App') === false){
+		fs.mkdirSync(APP_PATH + '\\App');
+		R3DITOR_movePercent(0, 50, 'Moving the new files...');
 		runExternalSoftware("cmd", ["/C", "xcopy", APP_PATH + "\\Update\\Extract\\R3ditor-master\\App\\*", APP_PATH + "\\App\\", '/h', '/i', '/c', '/k', '/e', '/r', '/y']);
 		var timer = setInterval(function(){
 			if (EXTERNAL_APP_RUNNING === false){
@@ -156,8 +156,8 @@ function R3DITOR_update_2(){
 }
 function R3DITOR_update_3(){
 	clearInterval(timer);
-	R3DITOR_movePercent(0, 75, "Cleaning some files...");
-	deleteFolderRecursive(APP_PATH + "\\Update");
+	R3DITOR_movePercent(0, 75, 'Cleaning some files...');
+	deleteFolderRecursive(APP_PATH + '\\Update');
 	var timer = setInterval(function(){
 		if (EXTERNAL_APP_RUNNING === false){
 			clearInterval(timer);
@@ -166,8 +166,8 @@ function R3DITOR_update_3(){
 	}, 50);
 }
 function R3DITOR_update_4(){
-	R3DITOR_movePercent(0, 100, "Update Ok!");
-	document.title = APP_NAME + " - Update Ok!";
+	R3DITOR_movePercent(0, 100, 'Update Ok!');
+	document.title = APP_NAME + ' - Update Ok!';
 	if (DESIGN_ENABLE_ANIMS === true){
 		$("#img-logo").fadeOut({duration: 2000, queue: false});
 		$("#btn_update_ok").fadeIn({duration: 500, queue: false});
@@ -175,7 +175,7 @@ function R3DITOR_update_4(){
 		$("#img-logo").css({'display': 'none'});
 		$("#btn_update_ok").css({'display': 'inline'});
 	}
-	$("#progress_window").css({"top": "528px", "height": "74px"});
+	$("#progress_window").css({'top': '528px', 'height': '74px'});
 	addLog('log', 'INFO - Click on Reload App to apply changes!');
 	scrollLog();
 }
