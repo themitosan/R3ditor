0eb1000af9fbea35bd7a8402fdfd1fea70d8799d
TheMitoSan <juliodogers@gmail.com>
2020-09-02 14:06:54 -0300

    a fix to fix the fix!

--- App/js/utils.js
+++ App/js/utils.js
@@ -486,7 +486,7 @@ function RDT_checkMap(path){
 				var cantReason = '';
 				var canImport = true;
 				var fName = getFileName(path).toUpperCase();
-				if (fName.slice(4, 5) !== '.' || fName.length !== 8){
+				if (fName.slice(4, 5) !== '.' || fName.length !== 4){
 					var ask = prompt('WARN - The filename are not set in RE3 pattern!\n\nPlease insert the stage and the map number below:\nExample: 10F (Stage 1, Map 0F - R10F)');
 					if (ask === null || ask.length !== 3){
 						canImport = false;
